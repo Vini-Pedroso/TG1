fced5b4e792a9b70ba7df55e893fa6b720e55352
VInicius Freitas Pedroso <129963034+Vini-Pedroso@users.noreply.github.com>
2023-10-20 16:14:54 -0300

    Update Cartas.py
    deck atualizado 16:14 de sexta

--- Cartas.py
+++ Cartas.py
@@ -8,22 +8,30 @@ class ErrorSeed(Exception):
 class Deck:
     def __init__(self, num_decks=1):
         self.num_decks = num_decks
+        self.cartas_disponiveis = self.baralhos()
+        self.seed = None
+        self.cards = self.baralhos()
         if num_decks > 6:
             self.num_decks = 6
             raise ErrorSeed("Número máximo de decks permitidos: 6\n Setando automáticamente o número de decks para 6")
         self.cards = self.baralhos()
+        with open("baralho.txt","w") as file:
+            for card in self.cards:
+                file.write(f"{card['valor']} de {card['naipe']}\n")
         
 
     def baralhos(self):
         naipes = ['Paus', 'Ouros', 'Copas', 'Espadas']
-        valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valete', 'Dama', 'Rei', 'Ás']
+        valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valete', 'Dama', 'Rei', 'As']
         baralho = [{'naipe': naipe, 'valor': valor} for naipe in naipes for valor in valores]
         return baralho * self.num_decks
     
 
-    def shuffle(self, seed=101):
-        if seed == 101:
-            random.seed(101)
+    def embaralhar(self, seed=None):
+        if seed is not None:
+            self.seed = seed
+        if self.seed is not None:
+            random.seed(seed)
             random.shuffle(self.cards)
         else:
             raise ErrorSeed("Seed invalida")
@@ -32,10 +40,12 @@ class Deck:
         with open("baralho.txt" , "r+") as file:
             lines = file.readlines()
             selected_card = lines[0]
-            lines.remove(selected_card) # remove a carta que foi pega 
-            file.seek(0) #escreve no primeiro espaço o baralho sem a carta selecionada
-            file.writelines(lines) 
-            return selected_card
+            lines.pop(0) # remove a carta que foi pega 
+            file.seek(0) #aponta para o primeiro espaço do arquivo
+            file.truncate() #tira tudo que tem no arquivo (precisa do seek antes)
+            file.writelines(lines) #reescreve sem a carta removida
+        naipe, valor = selected_card.strip().split(" de ")
+        return {'valor': valor, 'naipe': naipe}
         
 
     def print_deck(self):
@@ -49,6 +59,8 @@ class Deck:
         with open(file,"w") as file:
             for card in self.cards:
                 file.write(f"{card['valor']} de {card['naipe']}\n")
+
+
     def load_from_file(self,file):
         baralho = []
         with open(file,"r") as file :
@@ -59,13 +71,25 @@ class Deck:
                 baralho.append({'naipe': naipe, 'valor': valor})
         print(baralho)
         return baralho
-            
+    def get_deck_value(self, string) ->int : 
+        pass
+    
+    #def get_deck_value(self, string) ->int : 
+    #    valores = [2,3,4,5,6,7,8,9,10,10,10,11]
+    #    result = string.split("de")[0].strip()
+    #    #print(result)
+    #    if (result == "Valete") or  (result == "Dama") or (result == "Rei"):
+    #        return 10
+    #    elif (result == "As"):
+    #        return 11
+    #    else:
+    #        return int(result) 
 
         
 # Exemplo de uso:
 if __name__ == "__main__":
     deck = Deck(num_decks=1)  # Criar um deck com dois baralhos
-    deck.shuffle(seed=101)     # Embaralhar o deck com semente 42
+    deck.embaralhar(seed=101)     # Embaralhar o deck com semente 42
     deck.save_to_file("baralho.txt")  # Salvar o deck em um arquivo
 
     # Carregar o deck de um arquivo
@@ -75,8 +99,8 @@ if __name__ == "__main__":
     deck.print_deck()
     tamanho_decks = deck.get_deck_size()
     print("tamanho do baralho é: ", tamanho_decks)
-    print();print() ;print() 
-    deck.dar_carta() ;deck.dar_carta()
-    deck.load_from_file("baralho.txt")
-    #deck.print_deck()
-
+    print();print() ;print()     
+    #val = deck.get_deck_value("9 de copas")
+    #print(val)
+    
+    
