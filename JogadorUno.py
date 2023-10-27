4c8f664377afc5fe84f39f02b6ce3b39431d2371
VInicius Freitas Pedroso <129963034+Vini-Pedroso@users.noreply.github.com>
2023-10-27 00:31:21 -0300

    Jogador uno do dia 24/10/23

--- JogadorUno.py
+++ JogadorUno.py
@@ -1,64 +1,27 @@
 import random
-from jogador import Jogador  # Certifique-se de importar a classe Jogador do pacote correto.
+from jogador import Jogador
 from cartas import Deck
+
+
 class ErrorCPF(Exception):
     def __init__(self, msg):
         self.msg = msg
+
+class BaralhoVazioException(Exception):
+    pass
+
 class JogadorUno(Jogador):
-    def __init__(self, nome, cpf, condParada, deck):
-        super().__init__(nome, cpf, saldo=0)
+    def __init__(self, nome, cpf, saldo, game_number):
+        super().__init__(nome, cpf, saldo)
         self.cartas_mao = []
         self.cpf = str(cpf)
-        self.condParada = condParada
         if len(cpf) != 11:
-            raise ErrorCPF(f" {nome} ESTÁ COM CPF INVALIDO")
-        self.deck = deck
-    
-    def fazerJogada(self, carta_mesa):
-        jogadas_validas = [carta for carta in self.cartas_mao if carta['naipe'] == carta_mesa['naipe'] or carta['valor'] == carta_mesa['valor']]
-        valid_play = False
+            raise ErrorCPF(f"{nome} ESTÁ COM CPF INVALIDO")
+        self.game_number = game_number
+        self.output_filename = f'jogo_{self.game_number}.saida'
+        self.log_filename = f'jogo_{self.game_number}.log'
+        self.init_output_file()  # Inicialize o arquivo de saída
 
-        while not valid_play:
-            self.display_player_hand()
-            if self.is_winner():
-                self.report_winner()
-                return None  # Adicione um retorno aqui para indicar que o jogador não jogou uma carta válida
-
-            card_index = int(input(f"Vez do Jogador {self.nome}. Escolha uma carta para jogar (0 a {len(self.cartas_mao) - 1}): "))
-
-            if card_index >= 0 and card_index < len(self.cartas_mao):
-                selected_card = self.cartas_mao[card_index]
-
-                if self.is_valid_play(selected_card, carta_mesa):
-                    self.cartas_mao.remove(selected_card)
-                    carta_mesa = selected_card
-                    valid_play = True
-                else:
-                    print("Jogada inválida. Tente novamente.")
-            else:
-                print("Índice de carta inválido. Tente novamente.")
-        
-        return carta_mesa  # Retorne a carta jogada
-
-    def is_valid_play(self, card, carta_mesa):
-        if card['naipe'] == carta_mesa['naipe'] or card['valor'] == carta_mesa['valor']:
-            return True
-        return False
-
-    def display_player_hand(self):
-        print(f"Cartas de {self.nome}:")
-        for i, card in enumerate(self.cartas_mao):
-            print(f"{i}: {card['valor']} de {card['naipe']}")
-
-    def is_winner(self):
-        return len(self.cartas_mao) == 0
-
-    def report_winner(self):
-        with open("jogo_resultado.txt", "a") as file:
-            file.write(f"Vencedor: Jogador: {self.nome} CPF: {self.cpf} Número cartas: {len(self.cartas_mao)}\n")
-
-
-    # SOBRECARGA DE OPERADORES:
     def __gt__(self, other):
         return len(self.cartas_mao) > len(other.cartas_mao)
 
@@ -77,14 +40,59 @@ class JogadorUno(Jogador):
     def __ne__(self, other):
         return len(self.cartas_mao) != len(other.cartas_mao)
     
-    def comprar_carta(self):
-        carta_comprada = self.deck.dar_carta()
 
-        if self.is_valid_play(carta_comprada, self.current_card):
-            self.cartas_mao.append(carta_comprada)
-            self.deck.cards.remove(carta_comprada)
-        else:
-            self.deck.descartar_carta(carta_comprada)
+    def init_output_file(self):
+        with open(self.output_filename, 'w') as file:
+            file.write(f'Comecando jogo {self.game_number}\n')
+          
+    def log_error(self, error_message):
+        with open(self.log_filename, 'a') as file:
+            file.write(f'ERRO: {error_message}\n')
+
+    def report_round_start(self, round_number):
+        with open(self.output_filename, 'a') as file:
+            file.write(f'Comecando Rodada {round_number}\n')
+
+    def report_winner(self, winner):
+        with open(self.output_filename, 'a') as file:
+            file.write(f'Vencedor: Jogador: {winner.nome} CPF: {winner.cpf} '
+                       f'Numero cartas: {len(winner.cartas_mao)}\n')
 
     def atribuirCarta(self, carta):
-        self.cartas_mao.append(carta)
+        try:
+            self.cartas_mao.append(carta)
+        except BaralhoVazioException:
+            # Tratar a exceção e encerrar o jogo, se necessário
+            # Você pode definir a lógica apropriada para encerrar o jogo quando o baralho estiver vazio
+            self.encerrarJogo()
+
+
+    def fazerJogada(self, carta_mesa):
+        jogadas_validas = [carta for carta in self.cartas_mao if carta['naipe'] == carta_mesa['naipe'] or carta['valor'] == carta_mesa['valor']]
+
+        if len(jogadas_validas) > 0:
+            # O jogador tem pelo menos uma jogada válida
+            carta = jogadas_validas[0]
+            self.cartas_mao.remove(carta)
+            if not self.cartas_mao:
+                print(f"{self.nome} ganhou o jogo")
+            return carta
+
+        else:
+            # O jogador não tem jogadas válidas, então ele compra uma carta
+            carta_comprada = self.comprar_carta(carta_mesa)
+            print(f"{self.nome} comprou uma carta: {carta_comprada['valor']} de {carta_comprada['naipe']}")
+            if not self.cartas_mao:
+                print(f"{self.nome} ganhou o jogo")
+            return carta_mesa
+
+
+    def comprar_carta(self, carta_mesa):
+        meu_deck = Deck(seed=101) 
+        meu_deck.load_from_file("baralho.txt")  
+        nova_carta = meu_deck.dar_carta()
+        self.cartas_mao.append(nova_carta)
+        return nova_carta
+
+    def is_valid_play(self, card, carta_mesa):
+        return card['naipe'] == carta_mesa['naipe'] or card['valor'] == carta_mesa['valor']
