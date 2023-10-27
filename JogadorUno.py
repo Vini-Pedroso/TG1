8c13e76c3cf3fa8a63f3aabfb640bffc3501d87b
Gabriel Stefanello <85457993+GabrielMattos7@users.noreply.github.com>
2023-10-27 07:37:59 -0300

    Update JogadorUno.py

--- JogadorUno.py
+++ JogadorUno.py
@@ -1,6 +1,6 @@
 import random
-from jogador import Jogador
-from cartas import Deck
+from Jogador import Jogador
+from Cartas import Deck
 
 
 class ErrorCPF(Exception):
@@ -18,8 +18,8 @@ class JogadorUno(Jogador):
         if len(cpf) != 11:
             raise ErrorCPF(f"{nome} ESTÁ COM CPF INVALIDO")
         self.game_number = game_number
-        self.output_filename = f'jogo_{self.game_number}.saida'
-        self.log_filename = f'jogo_{self.game_number}.log'
+        self.output_filename = f'saidas/jogo_{self.game_number}.saida'
+        self.log_filename = f'saidas/jogo_{self.game_number}.log'
         self.init_output_file()  # Inicialize o arquivo de saída
 
     def __gt__(self, other):
@@ -42,19 +42,19 @@ class JogadorUno(Jogador):
     
 
     def init_output_file(self):
-        with open(self.output_filename, 'w') as file:
+        with open(self.output_filename, 'a+') as file:
             file.write(f'Comecando jogo {self.game_number}\n')
           
     def log_error(self, error_message):
-        with open(self.log_filename, 'a') as file:
+        with open(self.log_filename, 'a+') as file:
             file.write(f'ERRO: {error_message}\n')
 
     def report_round_start(self, round_number):
-        with open(self.output_filename, 'a') as file:
+        with open(self.output_filename, 'a+') as file:
             file.write(f'Comecando Rodada {round_number}\n')
 
     def report_winner(self, winner):
-        with open(self.output_filename, 'a') as file:
+        with open(self.output_filename, 'a+') as file:
             file.write(f'Vencedor: Jogador: {winner.nome} CPF: {winner.cpf} '
                        f'Numero cartas: {len(winner.cartas_mao)}\n')
 
