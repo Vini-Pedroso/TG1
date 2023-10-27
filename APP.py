e67f616c0b82fe28add073fd955a029612a881ec
Gabriel Stefanello <85457993+GabrielMattos7@users.noreply.github.com>
2023-10-27 07:37:17 -0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,78 +1,103 @@
-from cartas import Deck
-from jogadorUno import JogadorUno
-from uno import Uno
+from JogadorUno import JogadorUno
+from Uno import Uno
 import os
-
+import BlackJack
 class ErrorLine(Exception):
     def __init__(self, msg):
         self.msg = msg
 
-def main():
-    # Defina aqui o nome do arquivo que você deseja ler
-    arquivo = "jogo_6.txt"
+class ErrorJogo(Exception):
+    def __init__(self, msg="Jogo inválido"):
+        self.msg = msg
+        super().__init__(self.msg)
 
-    if os.path.exists(arquivo):
-        try:
-            game = load_game(arquivo)
-            if game is not None:
-                game.start_game()
-            else:
-                print("Jogo não suportado.")
-        except Exception as e:
-            print(f"Erro ao iniciar o jogo: {str(e)}")
-    else:
-        print(f"Arquivo {arquivo} não encontrado.")
+def main(): 
+    for game_number in range(1, 11):
+        arquivo = f"entradas/jogo_{game_number}.txt"
+        if os.path.exists(arquivo):
+            with open(arquivo, "r") as file:
+                game_info = file.readline().strip().split("--")
+                game_name = game_info[0]
+            try:
+                if game_name == "Blackjack":
+                    game = load_and_play_Blackjack_game(arquivo)
+                elif game_name == "Uno":
+                    print("Aqui")
+                    print(game_name)
+                    game = load_and_play_uno_game(arquivo)                
+                    game.start_game()
+                
+                #else:print("Jogo irreconhecivel")
+                
+                  
+                
+            except Exception as e:
+                print(f"Erro ao iniciar o jogo {game_number}: {str(e)}")
+        else:
+            print(f"Arquivo {arquivo} não encontrado.")
 
-def load_game(file_name):
-    with open(file_name, "r") as file:
+def get_winner_from_output(output_filename):
+    winner = None
+    with open(output_filename, "r") as file:
+        lines = file.readlines()
+        for line in reversed(lines):
+            if line.startswith("Vencedor"):
+                winner = line.strip().split(":")[1].strip()
+                break
+    return winner
+
+def load_and_play_uno_game(arquivo):
+    print(arquivo)
+    with open(arquivo, "r") as file:
         game_info = file.readline().strip().split("--")
+        
         game_name = game_info[0]
         
         if game_name == 'Uno':
-            return load_uno(game_info)
 
-        if len(game_info) != 5:
-            raise ErrorLine(f"Arquivo {file_name} não possui informações suficientes.")
+            num_players, num_decks, seed, rounds = game_info[1:]
+            #print(game_info)
+            players_info = [line.strip() for line in file.readlines()[1:]]
+            print(players_info[0])
+            num_players = int(num_players)
+            num_decks = int(num_decks)
+            seed = int(seed)
+            rounds = int(rounds)
+            players = []
+            print("aqui")
+            _ , game_number = arquivo.split("_")
+            game_number,_ = game_number.split(".")
+            print(game_number)
+            for player_info in players_info:
+                print(player_info)
+                nome, cpf = player_info.strip().split("--")
+                jogador = JogadorUno( nome, cpf, 0, game_number)
+                players.append(jogador)
+                print(jogador)
+            print(num_players)
+            print(num_decks)
+            print(seed)
+            print(rounds)
+            print()
+            print(game_number)
+            print(arquivo)
+            uno_game = Uno(arquivo,num_players, num_decks, seed, rounds, game_number)
+            
+            
+            uno_game.start_game()
 
-        nomeJogo, NumeroJogadores, NumeroBaralhos, seed, rodadas = game_info
-        NumeroJogadores = int(NumeroJogadores)
-        NumeroBaralhos = int(NumeroBaralhos)
-        seed = int(seed)
-        rodadas = int(rodadas)
+            # Obtenha o vencedor usando o método get_winner
+            winner = uno_game.get_winner()
+            print(f"Vencedor: {winner}")
+            return uno_game
+def load_and_play_Blackjack_game(arquivo):
 
-        jogadores = []
-
-        for line in file:
-            line = line.strip().split("--")
-            if len(line) < 2:
-                raise ErrorLine(f"Arquivo {file_name} contém informações de jogador inválidas.")
-            nome, cpf = line[0], line[1]
-            saldo = int(line[2]) if len(line) > 2 else 0
-            condParada = int(line[3]) if len(line) > 3 else 0
-            jogador = JogadorUno(nome, cpf, saldo, condParada)
-            jogadores.append(jogador)
-
-        activePlayers = [player for player in jogadores if player.saldo > 0]
-
-        return Uno(NumeroJogadores, activePlayers, NumeroBaralhos, seed, rodadas)
-    
-def load_uno(lines):
-    game_info = lines[0].strip().split("--")
-    num_players = int(game_info[1])
-    num_decks = int(game_info[2])
-    seed = int(game_info[3])
-    rounds = int(game_info[4])
-
-    players = []
-
-    for i in range(1, num_players + 1):
-        player_info = lines[i].strip().split("--")
-        nome = player_info[0]
-        cpf = player_info[1]
-        condParada = int(player_info[2])
-        players.append((nome, cpf, condParada))
-
-    return Uno(num_players, players, num_decks, seed, rounds)
-
+    with open(arquivo, "r") as file:
+        game_info = file.readline().strip().split("--")
+        game_name = game_info[0]
+        print(game_name)
+    if game_name == 'Blackjack':
+        BlackJack.inicia_BlackJack_jogo(arquivo)
 if __name__ == "__main__":
     main()
+
